58e44d38ce29db125cf6a998f9a43372ba18a7f3
Mostafa Mead <39874450+AimMead@users.noreply.github.com>
2018-06-04 08:00:19 +0200

    Add files via upload

---
+++ AccessTokenChecker.py
@@ -0,0 +1,21 @@
+import requests
+import time
+print("Coder -> Mostafa M. Mead")
+print("Github -> Github/AimMead")
+print("FB -> FB/100010261237574")
+print("")
+def checkToken(tokensTxt):
+	with open(tokensTxt) as f:
+		lines = f.read().split("\n")
+	count = 0
+	for token in lines:
+		url = requests.get("https://graph.facebook.com/me?access_token={0}".format(token))
+		source = url.content
+		count += 1
+		if "first_name" in source.decode("utf-8"):
+			goodTokens = open("goodTokens.txt" , 'a+')
+			goodTokens.write(token + "\n")
+			goodTokens.close()
+			print("Checking Token Number -> {0}".format(count) , end="\r")
+checkToken(input("Enter Tokens Path : "))
+time.sleep(500)
